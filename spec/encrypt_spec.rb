fd9cc05d573edbe0370f761527a909451364614c
Sven Fuchs <me@svenfuchs.com>
2015-09-24 16:16:26 +0200

    add travis-core legacy compat test

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -4,7 +4,7 @@ describe Travis::Encrypt do
   let(:string)    { 'travis' }
   let(:encrypted) { encrypt(string, options) }
   let(:decrypted) { decrypt(encrypted, options) }
-  let(:options)   { { key: 'abcd' * 8 } }
+  let(:options)   { { key: 'secret' * 10 } }
 
   it 'can decrypt an encrypted string' do
     expect(decrypted).to eql(string)
@@ -17,5 +17,10 @@ describe Travis::Encrypt do
   it 'does not prefix the encrypted string when opted out' do
     options[:use_prefix] = false
     expect(encrypted[0..7]).to_not eql(described_class::PREFIX)
+  end
+
+  describe 'travis-core legacy compat' do
+    let(:encrypted) { '--ENCR--q/BkAx83j9FaMqw0RR2mqzBmMjI1ZGFjMGI3YTY3ZDc=' }
+    it { expect(decrypted).to eq('foo') }
   end
 end
